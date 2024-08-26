52474c180aceffd9420afa91cfaee08df7bb599d
artem <artemqq5@gmail.com>
2024-08-26 14:24:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from flask import Flask, request
+from gevent.pywsgi import WSGIServer
 
 from data.repository.invoices import InvoiceRepository
 from data.repository.users import UserRepository
@@ -43,8 +44,14 @@ async def invoice_completed():
             # check is invoice exist and simular value in invoice
             invoice = InvoiceRepository().invoice(data['order']['id'])
             # print(invoice)
-            if not invoice or not float(data['order']['value']) == invoice['value'] or invoice['status'] == "COMPLETE":
-                print("ERROR: Value Invoice no the same or status already completed")
+            if not invoice:
+                print("ERROR: Invoice not exist in database")
+                return "Bad request", 400
+            if not float(data['order']['value']) == invoice['value']:
+                print("ERROR: Value Invoice no the same")
+                return "Bad request", 400
+            if not invoice['status'] != "INIT":
+                print("ERROR: Invoice status already changed from INIT")
                 return "Bad request", 400
 
             default_properties = DefaultBotProperties(parse_mode=ParseMode.HTML)
@@ -89,30 +96,30 @@ async def invoice_completed():
 # }
 
 
-# if __name__ == '__main__':
-    # payload1 = {
-    #     "order": {
-    #     "id": "08d585b1-86c6-496d-b493-ab17a574fdd6",
-    #     "currency": "UAH",
-    #     "value": "200",
-    #     "expected_amount": "200",
-    #     "status": "DECLINED",
-    #     "external_order_id": "423se231-f351-234g-g324-g35gd3452f45",
-    #     "created_at": "2022-03-22 14:01:34",
-    #     "completed_at": null,
-    #     "acquiring_url": "https://merchant.pay.whitepay.com/fiat-order/08d585b1-86c6-496d-b493-ab17a574fdd6",
-    #     "is_internal": false
-    #   },
-    #   "event_type": "order::declined"
-    # }
+if __name__ == '__main__':
+    payload1 = {
+        "order": {
+        "id": "1f336422-8b84-442d-a085-1b06d6316c3f",
+        "currency": "USDT",
+        "value": "5",
+        "expected_amount": "5",
+        "status": "COMPLETED",
+        "external_order_id": "423se231-f351-234g-g324-g35gd3452f45",
+        "created_at": "2022-03-22 14:01:34",
+        "completed_at": "null",
+        "acquiring_url": "https://merchant.pay.whitepay.com/fiat-order/08d585b1-86c6-496d-b493-ab17a574fdd6",
+        "is_internal": "false"
+      },
+      "event_type": "order::completed"
+    }
 
-    #
-    # # Серіалізація JSON payload і кодування ключа в байти
-    # payload_json1 = json.dumps(payload1, separators=(',', ':'))  # Використовуйте ті ж сепаратори, що і в JavaScript
-    # signature1 = hmac.new(WHITE_PAY_WEBHOOK_TOKEN.encode('utf-8'), payload_json1.encode('utf-8'),
-    #                       hashlib.sha256).hexdigest()
 
-    # print(signature1)
-    # app.run(threaded=True)
-    # http_server = WSGIServer(("0.0.0.0", 5100), app)
-    # http_server.serve_forever()
+    # Серіалізація JSON payload і кодування ключа в байти
+    payload_json1 = json.dumps(payload1, separators=(',', ':'))  # Використовуйте ті ж сепаратори, що і в JavaScript
+    signature1 = hmac.new(WHITE_PAY_WEBHOOK_TOKEN.encode('utf-8'), payload_json1.encode('utf-8'),
+                          hashlib.sha256).hexdigest()
+
+    print(signature1)
+    app.run(threaded=True)
+    http_server = WSGIServer(("0.0.0.0", 5100), app)
+    http_server.serve_forever()
